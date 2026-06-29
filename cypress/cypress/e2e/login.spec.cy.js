5606a2ed50cbcd20b9248d3fa9008373d384217b
Doque <brunodoques@outlook.com>
2026-06-29 20:10:42 -0300

    ''

---
+++ cypress/cypress/e2e/login.spec.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+context('login', () => {
+
+    it(' login successfully', () => {
+
+
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    })
+
+    it('login invalid '), () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    }
+})
